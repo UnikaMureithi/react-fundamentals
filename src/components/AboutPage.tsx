d465a7984b029f5338a162162e000bb624ee8ad6
Unika Mureithi <unikamureithi@gmail.com>
2024-03-31 17:57:04 +0300

    feat: display data

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+const user = {
+    name:"Unika Mureithi",
+    imageUrl: "https://imgur.com/u5OUfBF.jpg",
+    imageSize: 120,
+};
 
 function AboutPage(){
     return(
         <>
+
         <h1>About Me</h1>
-        <p>Hello, my name is Unika Mureithi, and I am learning React.</p>
+        <p>Hello, my name is {user.name}, and I am learning React.</p>
+        <img
+        src = {user.imageUrl}
+        style = {{ width:user.imageSize,
+        height:user.imageSize }} />
+
         </>
     );
 }
