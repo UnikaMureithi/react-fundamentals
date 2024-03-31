dfc1d508f2591c34e66cd4495c6c93a7e4134d6a
Unika Mureithi <unikamureithi@gmail.com>
2024-03-31 18:57:40 +0300

    feat: update the screen using useState

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useState } from 'react';
 
 function Button (){
+    const [count, setCount] = useState(0);
 
     function handleClick(){
-        alert('You clicked me!');
+        //alert('You clicked me!');
+        setCount(count + 1);
     }
 
     return(
-        <button onClick={handleClick}>New Button</button>
+        <button onClick={handleClick}>Clicked {count} times</button>
     );
 }
 
