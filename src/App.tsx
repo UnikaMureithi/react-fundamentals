eb7c21cb9ca8a9d3ace26bb62d1d63614105e179
Unika Mureithi <unikamureithi@gmail.com>
2024-03-31 17:14:34 +0300

    feat: write markup with JSX

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
+import AboutPage from './components/AboutPage';
 
 function App() {
   return (
     <div>
       <h1>Hello! This is my first button</h1>
       <Button/>
+      <AboutPage/>
     </div>
   );
 }
---
+++ src/components/AboutPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutPage(){
+    return(
+        <>
+        <h1>About Me</h1>
+        <p>Hello, my name is Unika Mureithi, and I am learning React.</p>
+        </>
+    );
+}
+
+export default AboutPage;
